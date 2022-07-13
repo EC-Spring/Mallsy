9e86c12538bb00acb7bd58158104bf6540d829b8
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2022-07-13 14:40:46 +0900

    Commenting for Pull Requests

--- src/main/java/org/ec/mallsy/domain/Item/predicate/ItemClassPredicate.java
+++ src/main/java/org/ec/mallsy/domain/Item/predicate/ItemClassPredicate.java
@@ -6,24 +6,24 @@ import org.ec.mallsy.domain.Item.dto.request.ItemGetByClassReq;
 import org.ec.mallsy.domain.Item.entity.QItem;
 
 public class ItemClassPredicate {
-    public static Predicate search(ItemGetByClassReq itemGetByClassReq) {
-        QItem qItem = QItem.item;
-
-        String largeClass = itemGetByClassReq.getLargeClass();
-        String mediumClass = itemGetByClassReq.getMediumClass();
-        String smallClass = itemGetByClassReq.getSmallClass();
-
-        BooleanBuilder builder = new BooleanBuilder();
-        if (!largeClass.equals("")) {
-            builder.and(qItem.largeClass.eq(largeClass));
-        }
-        if (!mediumClass.equals("")) {
-            builder.and(qItem.mediumClass.eq(mediumClass));
-        }
-        if (!smallClass.equals("")) {
-            builder.and(qItem.smallClass.eq(smallClass));
-        }
-
-        return builder;
-    }
+//    public static Predicate search(ItemGetByClassReq itemGetByClassReq) {
+//        QItem qItem = QItem.item;
+//
+//        String largeClass = itemGetByClassReq.getLargeClass();
+//        String mediumClass = itemGetByClassReq.getMediumClass();
+//        String smallClass = itemGetByClassReq.getSmallClass();
+//
+//        BooleanBuilder builder = new BooleanBuilder();
+//        if (!largeClass.equals("")) {
+//            builder.and(qItem.largeClass.eq(largeClass));
+//        }
+//        if (!mediumClass.equals("")) {
+//            builder.and(qItem.mediumClass.eq(mediumClass));
+//        }
+//        if (!smallClass.equals("")) {
+//            builder.and(qItem.smallClass.eq(smallClass));
+//        }
+//
+//        return builder;
+//    }
 }
