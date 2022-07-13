4aa55adccf86a3c5e135a90c917f3d98ad66125b
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2022-07-13 15:35:18 +0900

    #3 - Create Item Test template

---
+++ src/test/java/org/ec/mallsy/domain/Item/controller/ItemControllerTest.java
@@ -0,0 +1,38 @@
+package org.ec.mallsy.domain.Item.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getSummarizedItemsByClassAsPage() {
+    }
+
+    @Test
+    void getItemById() {
+    }
+
+    @Test
+    void postItem() {
+    }
+
+    @Test
+    void putItem() {
+    }
+
+    @Test
+    void deleteItem() {
+    }
+}
---
+++ src/test/java/org/ec/mallsy/domain/Item/service/impl/ItemServiceImplTest.java
@@ -0,0 +1,28 @@
+package org.ec.mallsy.domain.Item.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemServiceImplTest {
+
+    @Test
+    void getSummarizeItemsAsPage() {
+    }
+
+    @Test
+    void getItemById() {
+    }
+
+    @Test
+    void deleteItemById() {
+    }
+
+    @Test
+    void putItem() {
+    }
+
+    @Test
+    void postItem() {
+    }
+}
